f2ece60168c0927d93f19ea99e9562b124ed4f09
Yury_Home-PC_Win10_worked <yury.connect@gmail.com>
2024-11-06 00:01:16 +0300

    initial commit // GПервый коммит

---
+++ src/test/java/ru/itmentor/spring/rest/template/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.itmentor.spring.rest.template;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
